0fcb5089f584d5dc6a9fabbccc55c01800f5e984
jhovin <jhovin.bonifacio@tecsup.edu.pe>
2020-06-14 14:56:31 -0500

    primer commit

---
+++ Lab11MVVM/Lab11MVVM/Estilos/SharedResources.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Lab11MVVM.Estilos
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get
+            {
+                //return Color.White;
+                return Color.FromArgb(0xff, 0xa5, 0);
+            }
+        }
+    }
+}
